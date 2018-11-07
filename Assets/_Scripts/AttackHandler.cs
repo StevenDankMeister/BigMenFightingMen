46ab6b04cb5678dfae6c79ad3f2130877c900db7
DESKTOP-IB7C6IK\Steven <thedankpure@gmail.com>
2018-11-08 05:22:50 +0900

    forgot to add files xd

---
+++ Assets/_Scripts/AttackHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackHandler : MonoBehaviour {
+
+    public static AttackHandler attackHandler;
+
+	// Use this for initialization
+	void Start () {
+        attackHandler = GetComponent<AttackHandler>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void createAttack(Vector3 attackPos, GameObject attackPrefab, string player)
+    {
+        GameObject attack = Instantiate(attackPrefab, attackPos, Quaternion.identity);
+        attack.tag = player;
+    }
+}
---
+++ Assets/_Scripts/SpriteHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteHandler : MonoBehaviour {
+
+    public static SpriteHandler spriteHandler;
+
+	void Start () {
+        spriteHandler = GetComponent<SpriteHandler>();
+	}
+	
+	public void characterDirection(bool facingLeft, bool facingRight, GameObject ch)
+    {
+        SpriteRenderer sprite = ch.GetComponent<SpriteRenderer>();
+
+        if (facingLeft)
+        {
+            sprite.flipX = true;
+        }
+        else if (facingRight)
+        {
+            sprite.flipX = false;
+        }
+    }
+
+    public void setHitbox(GameObject ch)
+    {
+        BoxCollider2D hitbox = ch.GetComponent<BoxCollider2D>();
+        SpriteRenderer sprite = ch.GetComponent<SpriteRenderer>();
+
+        Vector3 hitbox_size = sprite.sprite.bounds.size;
+        hitbox.size = hitbox_size;
+    }
+
+
+}
