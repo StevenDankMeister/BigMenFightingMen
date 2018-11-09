c25650de93663bfea251e6dbb723c91901d817c9
Simon <simonahtx16@gmail.com>
2018-11-09 11:23:23 +0100

    added menu
    scenechanger

---
+++ Assets/_Scripts/SceneChanger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+public void changeMenuScene(string sceneName)
+    {
+
+        SceneManager.LoadScene(sceneName);
+
+    }
+
+
+
+}
