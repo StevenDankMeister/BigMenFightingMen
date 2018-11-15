716186a662b32a3f90b38b684204af18085b2909
Simon <simonahtx16@gmail.com>
2018-11-15 13:01:24 +0100

    menu xD
    værsgo

---
+++ Assets/SceneChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SceneChange : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void changeMenuScene(string sceneName)
+    {
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene("Arena");
+        }
+
+
+	}
+}
